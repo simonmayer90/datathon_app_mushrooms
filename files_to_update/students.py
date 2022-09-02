d57c2a3f94114586c854b0cc94c9d1270c5a97ac
JC <joanclaverol@gmail.com>
2022-09-02 08:46:30 +0200

    modified students list

--- files_to_update/students.py
+++ files_to_update/students.py
@@ -1,10 +1,16 @@
 students = [
-    'monika',
-    'ben',
-    'hana',
-    'vinita',
-    'ali',
-    'guillem',
-    'sabine',
-    'joan'
+    "Impure Regressors", # Sebastian & Aslam
+    "Daniel_A",
+    "Daniel_D",
+    "Huy",
+    "Icaro",
+    "Ilkay",
+    "Kayla",
+    "Komal",
+    "Nabil",
+    "Orjada",
+    "Peter",
+    "Sherwan",
+    "ToSi", # Tobias & Simon
+    "wbs_bot"
 ]
