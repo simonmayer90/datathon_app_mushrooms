fa7d323fbd4655d2fa7bb4b814c1ae1f45ddbba6
hanaamulic <hana.amulic@gmail.com>
2022-11-02 16:23:00 +0100

    tests and different measures

---
+++ files_to_update/.ipynb_checkpoints/students-checkpoint.py
@@ -0,0 +1,15 @@
+students = [
+    "Impure Regressors", # Sebastian & Aslam & Komal
+    "Daniel_A",
+    "Daniel_D",
+    "Huy",
+    "Icaro",
+    "Ilkay",
+    "Kayla",
+    "Nabil",
+    "Orjada",
+    "Peter",
+    "Sherwan",
+    "ToSi", # Tobias & Simon
+    "wbs_bot"
+]
