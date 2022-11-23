b0579d4eadd5be94d78c900d8651e0a13ff4fb85
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2022-11-23 15:34:51 +0100

    removed pickle file and students name check

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from files_to_update.students import students
+# from files_to_update.students import students
 from src.pd_functions import *
 
 # path to results
@@ -9,49 +9,46 @@ RESULTS_PATH = 'data/results.csv'
 # introduce participant name
 text_input_container = st.empty()
 text_input_container.text_input(
-    "Introduce participant name: ", 
+    "Introduce participant name: ",
     key="text_input"
-    )
-if st.session_state.text_input != "": 
-    if st.session_state.text_input in students:
-        text_input_container.empty()
-        st.info('Participant name ' + st.session_state.text_input)
-    
+)
+if st.session_state.text_input != "":
+    # if st.session_state.text_input in students:
+    text_input_container.empty()
+    st.info('Participant name ' + st.session_state.text_input)
 
-        # upload results file
-        uploaded_file = st.file_uploader("Choose a file")
+    # upload results file
+    uploaded_file = st.file_uploader("Choose a file")
 
-        if uploaded_file is not None and st.session_state.text_input != "":
-            try: 
-                # prepare test file
-                test = get_ready_test(RESULTS_PATH, uploaded_file)
+    if uploaded_file is not None and st.session_state.text_input != "":
+        try:
+            # prepare test file
+            test = get_ready_test(RESULTS_PATH, uploaded_file)
 
+            # get participant accuracy and prepare data for leaderboard
+            participant_results = get_metrics(RESULTS_PATH, test)
 
-                # get participant accuracy and prepare data for leaderboard
-                participant_results = get_metrics(RESULTS_PATH, test)
+            st.success('Dataframe uploaded successfully!')
+            # print participant results
+            st.title('Participant results')
+            st.dataframe(participant_results)
 
-                st.success('Dataframe uploaded successfully!')
-                # print participant results
-                st.title('Participant results')
-                st.dataframe(participant_results)
-                
-                # update all submissions
-                try: 
-                    update_submissions(participant_results)
-                    # plot_submissions(st.session_state.text_input)
-                except: 
-                    participant_results.to_pickle('files_to_update/submissions.pkl')
+            # update all submissions
+            try:
+                update_submissions(participant_results)
+                # plot_submissions(st.session_state.text_input)
+            except:
+                participant_results.to_pickle(
+                    'files_to_update/submissions.pkl')
 
-            except: 
-                st.error('The file has a wrong format, please, review it and load it again.')
+        except:
+            st.error(
+                'The file has a wrong format, please, review it and load it again.')
 
-        
-    
-    else: 
-        st.error("Please, introduce the your correct student name.")
-        
-try: 
+    # else:
+    #     st.error("Please, introduce the your correct student name.")
+
+try:
     show_leaderboard()
 except:
     st.write("There are no submissions yet.")
-    
--- src/pd_functions.py
+++ src/pd_functions.py
@@ -4,110 +4,135 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-def get_ready_test(RESULTS_PATH: str, uploaded_file):
 
+def get_ready_test(RESULTS_PATH: str, uploaded_file):
+    """
+    This function prepares the test file to be evaluated and 
+    check if it has the correct format.
+    """
     results = pd.read_csv(RESULTS_PATH)
-    results.columns = ['id','real']
-    
+    results.columns = ['id', 'real']
+
     test = pd.read_csv(uploaded_file)
-    if (test.columns.to_list() != ['Id','poisonous']):
+    if (test.columns.to_list() != ['Id', 'poisonous']):
         st.error('Column names must match "Id" and "poisonous" - case sensitive!')
         return 0
-    if(test.shape != (1625, 2) ):
+    if(test.shape != (1625, 2)):
         st.error('Your file should contain 1625 rows and 2 columns')
         return 0
-    if((test.poisonous.unique().tolist() != [0,1] ) & (test.poisonous.unique().tolist() != [1,0] ) & (test.poisonous.unique().tolist() != [1] ) & (test.poisonous.unique().tolist() != [0] ) ):
+    if((test.poisonous.unique().tolist() != [0, 1]) &
+        (test.poisonous.unique().tolist() != [1, 0]) &
+        (test.poisonous.unique().tolist() != [1]) &
+            (test.poisonous.unique().tolist() != [0])):
         st.error('Predictions should only have values of 0 and 1')
         return 0
-    if( (test.Id == results.id).sum() != 1625):
-        st.error("Your Id column might be wrong or mixed up. You should have same Id's as the test file. Order of Id's should also be the same.")
+    if((test.Id == results.id).sum() != 1625):
+        st.error(
+            "Your Id column might be wrong or mixed up. " +
+            "You should have same Id's as the test file. " +
+            "Order of Id's should also be the same.")
         return 0
-    test.columns = ['id','preds']
-    
+    test.columns = ['id', 'preds']
+
     return (
-    test
+        test
         .assign(
-            id = lambda df_: df_['id'].astype('int32'),
-            preds = lambda df_: df_['preds'].astype('object'),
-            )
+            id=lambda df_: df_['id'].astype('int32'),
+            preds=lambda df_: df_['preds'].astype('object'),
         )
+    )
 
 
 def get_metrics(RESULTS_PATH: str, test: pd.DataFrame):
-
+    """
+    This function calculates the metrics for the test file.
+    Metrics are: Accuracy, Recall, Deaths, Edible but uneaten.
+    """
     results = pd.read_csv(RESULTS_PATH)
-    results.columns = ['id','real']
+    results.columns = ['id', 'real']
 
     row_evaoluation = (
-    results
+        results
         .assign(
-            id = lambda df_: df_['id'].astype('int32'),
-            real = lambda df_: df_['real'].astype('object')
+            id=lambda df_: df_['id'].astype('int32'),
+            real=lambda df_: df_['real'].astype('object')
         )
         .merge(test, how='left', on='id')
         .assign(
-            tp = lambda df_: np.where((df_['real']==1) & (df_['preds']==1), True, False),
-            correct = lambda df_: df_['real'] == df_['preds'], 
-            fn = lambda df_: np.where((df_['real']==1) & (df_['preds']==0), True, False),
-            opportunity_cost = lambda df_: np.where((df_['real']==0) & (df_['preds']==1), True, False) 
-            )
-        .agg({
-            'tp':'sum',
-            'correct':'sum',
-            'fn':'sum',
-            'opportunity_cost':'sum'
-            })
+            tp=lambda df_: np.where((df_['real'] == 1) & (
+                df_['preds'] == 1), True, False),
+            correct=lambda df_: df_['real'] == df_['preds'],
+            fn=lambda df_: np.where((df_['real'] == 1) & (
+                df_['preds'] == 0), True, False),
+            opportunity_cost=lambda df_: np.where(
+                (df_['real'] == 0) & (df_['preds'] == 1), True, False)
         )
+        .agg({
+            'tp': 'sum',
+            'correct': 'sum',
+            'fn': 'sum',
+            'opportunity_cost': 'sum'
+        })
+    )
 
     return (
-    pd.DataFrame([[
-        st.session_state.text_input, 
-        row_evaoluation['tp']/(row_evaoluation['tp'] + row_evaoluation['fn']),  
-        row_evaoluation['correct']/results.shape[0],
-        row_evaoluation['fn'],
-        row_evaoluation['opportunity_cost'],
-        pd.Timestamp.now()
-        ]], 
-        columns=['Participant','Recall','Accuracy','Deaths','Edible but uneaten','submission_time'])
-        )
+        pd.DataFrame([[
+            st.session_state.text_input,
+            row_evaoluation['tp'] /
+            (row_evaoluation['tp'] + row_evaoluation['fn']),
+            row_evaoluation['correct']/results.shape[0],
+            row_evaoluation['fn'],
+            row_evaoluation['opportunity_cost'],
+            pd.Timestamp.now()
+        ]],
+            columns=['Participant', 'Recall', 'Accuracy', 'Deaths', 'Edible but uneaten', 'submission_time'])
+    )
 
 
-def plot_submissions(): 
-    
+def plot_submissions():
+    """
+    This function plots the submissions over time given the 
+    submissions.pkl file."""
+
     participant_submissions = (
         pd.read_pickle('files_to_update/submissions.pkl')
-            .query('Participant == @st.session_state.text_input')
-            .filter(['submission_time','Recall'])
-            .set_index('submission_time')
-            .copy()
-        )
+        .query('Participant == @st.session_state.text_input')
+        .filter(['submission_time', 'Recall'])
+        .set_index('submission_time')
+        .copy()
+    )
     if participant_submissions.shape[0] > 1:
         st.line_chart(participant_submissions)
-    else: 
+    else:
         st.success('Congratulations on your first submission!')
 
 
 def update_submissions(participant_results: pd.DataFrame):
+    """
+    This function updates the submissions.pkl file with the
+    new results."""
 
     (
         pd.concat([
-            pd.read_pickle('files_to_update/submissions.pkl'), 
+            pd.read_pickle('files_to_update/submissions.pkl'),
             participant_results
         ])
         .to_pickle('files_to_update/submissions.pkl')
     )
 
 
-def show_leaderboard(): 
+def show_leaderboard():
+    """
+    This function shows the leaderboard given the submissions.pkl"""
 
     st.title('LEADERBOARD')
 
     st.dataframe(
-    pd.read_pickle('files_to_update/submissions.pkl')
-        .assign(Attempts = lambda df_: df_.groupby('Participant')['Participant'].transform('count'))
-        .sort_values(['Recall','Accuracy'], ascending=[False, False])
+        pd.read_pickle('files_to_update/submissions.pkl')
+        .assign(Attempts=lambda df_: df_.groupby('Participant')['Participant'].transform('count'))
+        .sort_values(['Recall', 'Accuracy'], ascending=[False, False])
         .drop_duplicates(['Participant'], keep='first')
-        .assign(position = lambda df_: range(1, len(df_)+1)) 
-        .set_index('position')  
-        .filter(['Participant','Recall','Accuracy','Deaths','Edible but uneaten','Attempts'])
+        .assign(position=lambda df_: range(1, len(df_)+1))
+        .set_index('position')
+        .filter(['Participant', 'Recall', 'Accuracy', 'Deaths', 'Edible but uneaten', 'Attempts'])
     )
